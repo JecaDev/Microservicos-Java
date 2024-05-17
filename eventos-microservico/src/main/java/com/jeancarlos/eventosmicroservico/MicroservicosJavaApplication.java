08efce5ed42b355d354ab3a7e85b3fb10ed5c437
Jean Carlos <jeanmacedo1302@gmail.com>
2024-05-17 12:26:41 -0300

    Adição do projeto Spring

---
+++ eventos-microservico/src/main/java/com/jeancarlos/eventosmicroservico/MicroservicosJavaApplication.java
@@ -0,0 +1,13 @@
+package com.jeancarlos.eventosmicroservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicosJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicosJavaApplication.class, args);
+	}
+
+}
---
+++ eventos-microservico/src/test/java/com/jeancarlos/eventosmicroservico/MicroservicosJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeancarlos.eventosmicroservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicosJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
